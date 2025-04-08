4a37750b43958f1e68a5ebd01e9f96b332b84400
Mayra Landa <mlandae16@gmail.com>
2025-04-08 13:48:08 -0700

    add changes to fix deploy

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 const express = require("express");
 const path = require("path");
-const indexRoutes = require
-('./routes/index');
-const newMessageRoutes = require('./routes/newMessages');
-const messageRoutes = require('./routes/messages');
+const indexRoutes = require("./routes/index");
+const newMessageRoutes = require("./routes/newMessages");
+const messageRoutes = require("./routes/messages");
 const app = express();
 const port = process.env.PORT || 3000;
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-user: process.env.PGUSER,
-host: process.env.PGHOST,
-database: process.env.PGDATABASE,
-password: process.env.PGPASSWORD,
-port: process.env.PGPORT,
-ssl: {
-  rejectUnauthorized: false,
-}
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 });
 
-if (process.env.NODE_ENV === "development"){
-  pool.options.ssl = false;
-}
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   req.pool = pool;
   next();
-})
+});
 app.use("/", indexRoutes);
 app.use("/new", newMessageRoutes);
-app.use("/", messageRoutes
-);
+app.use("/", messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+});
 
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
